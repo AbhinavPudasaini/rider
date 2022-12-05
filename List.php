189c23bf60b86e30214df60b2f07fc466cac7cf1
AbhinavPudasaini <91964955+AbhinavPudasaini@users.noreply.github.com>
2022-12-05 10:25:25 +0545

    Add files via upload

---
+++ List.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Table</title>
+</head>
+<body>
+<a href="{{url('/create')}}"><button>Create</button></a>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Age</th>
+            <th>Image</th>
+        </tr>
+        @foreach($students as $student)
+            <tr>
+                <td>{{$student->name}}</td>
+                <td>{{$student->address}}</td>
+                <td>{{$student->age}}</td>
+                <td><img src="{{asset('storage/image/'.$student->image)}}"></td>
+                <td><a href="{{url('/edit/'.$student->id)}}">Edit</a></td>
+                <td><a href="{{url('/delete/'.$student->id)}}" onclick="return confirm('Are you sure you want to delete?')">Delete</a> </td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+</html>
